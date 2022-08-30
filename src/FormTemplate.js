54a5f2cbaea3df719a13c0ca98307045b3336324
emericgautier <emericgtr@gmail.com>
2022-08-30 20:23:17 +0200

    Create a template form

--- src/FormTemplate.js
+++ src/FormTemplate.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const FormTemplate = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    const formMess = document.querySelector(".form-message");
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          // console.log(result.text);
+          form.current.reset();
+          formMess.innerHTML = "<p class='success'>Message envoyé !</p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        },
+        (error) => {
+          // console.log(error.text);
+          formMess.innerHTML =
+            "<p class='error'>Une erreur s'est produite, veuillez réessayer</p>";
+
+          setTimeout(() => {
+            formMess.innerHTML = "";
+          }, 2500);
+        }
+      );
+  };
+
   return (
-    <div>
-      <h1>FORM</h1>
+    <div className="form-container">
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Nom</label>
+        <input type="text" name="name" autoComplete="off" required />
+        <label>Email</label>
+        <input type="email" name="email" autoComplete="off" required />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Envoyer" />
+      </form>
+      <div className="form-message"></div>
     </div>
   );
 };
